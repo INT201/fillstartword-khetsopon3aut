42ec3b8c879931f0716d16bd9d28b098ae889235
Khetsopon <khetsopon.kaew@kmutt.ac.th>
2022-09-11 16:44:34 +0700

    test

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,7 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  return startWord+word
 }
-module.exports = fillStartWord
+// module.exports = fillStartWord
+console.log(fillStartWord('hi','mo'))
