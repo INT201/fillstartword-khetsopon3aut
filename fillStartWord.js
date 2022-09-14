961ffdb96ddcf6a5fdde3d8364db1a619c77e295
Khetsopon <khetsopon.kaew@kmutt.ac.th>
2022-09-14 20:40:19 +0700

    suay

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,15 @@
-// const { template } = require('@babel/core')
+const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  return startWord+word
+  if(word===null|| word===undefined){
+  return undefined
+  }else{
+  if(word.includes(startWord)){//ถ้าwordมีstarrwordอยู่ข้างใน
+  return word
+  } else{
+    return startWord+word
+  }
 }
-// module.exports = fillStartWord
+}
+module.exports = fillStartWord
 console.log(fillStartWord('hi','mo'))
